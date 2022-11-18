f08da628b462a8ba3adb3ace1f13e46b0aaf92a3
j.adami <julien.adami@etu.iut-tlse3.fr>
2022-11-18 14:26:54 +0100

    Initial commit des sources M. Roussilles

---
+++ src/fr/rodez3il/a2022/mrmatt/sources/Niveau.java
@@ -0,0 +1,80 @@
+package fr.rodez3il.a2022.mrmatt.sources;
+
+import fr.rodez3il.a2022.mrmatt.sources.objets.ObjetPlateau;
+
+public class Niveau {
+	
+	// Les objets sur le plateau du niveau
+	private ObjetPlateau[][] plateau;
+	// Position du joueur
+	private int joueurX;
+	private int joueurY;
+	
+  // Autres attributs que vous jugerez nécessaires...
+  
+	/**
+	 * Constructeur public : crée un niveau depuis un fichier.
+	 * @param chemin .....
+	 * @author .............
+	 */
+	public Niveau(String chemin) {
+
+		chargerNiveau(chemin);
+	}
+
+	private void chargerNiveau(String chemin) {
+	}
+
+	/**
+	 * Javadoc à réaliser...
+	 */
+	private void echanger(int sourceX, int sourceY, int destinationX, int destinationY) {
+    // ........
+	}
+
+	/**
+	 * Produit une sortie du niveau sur la sortie standard.
+	 * ................
+	 */
+	public void afficher() {
+    // TODO
+	}
+
+  // TODO : patron visiteur du Rocher...
+	public void etatSuivantVisiteur(Rocher r, int x, int y) {
+    
+	}
+
+	/**
+	 * Calcule l'état suivant du niveau.
+	 * ........
+	 * @author 
+	 */
+	public void etatSuivant() {
+    // TODO
+	}
+
+
+  // Illustrez les Javadocs manquantes lorsque vous coderez ces méthodes !
+  
+	public boolean enCours() {return false;}
+
+  // Joue la commande C passée en paramètres
+	public boolean jouer(Commande c) {
+	}
+
+	/**
+	 * Affiche l'état final (gagné ou perdu) une fois le jeu terminé.
+	 */
+	public void afficherEtatFinal() {
+	}
+
+	/**
+	 */
+	public boolean estIntermediaire() {return false;}
+
+  // Code pour empêcher la compilation
+
+
+
+}
