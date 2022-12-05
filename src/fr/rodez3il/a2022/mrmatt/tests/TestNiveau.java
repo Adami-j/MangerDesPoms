a3389376db77f760a45a69ca4be3d66cf5daf016
juju <julien.adami@etu.iut-tlse3.fr>
2022-12-05 16:19:26 +0100

    ajout methodes test  testJouerCommandeVersMur(); testPositionPostJouer(); deplacementPossibleEtDeplacer(); testGagner();

--- src/fr/rodez3il/a2022/mrmatt/tests/TestNiveau.java
+++ src/fr/rodez3il/a2022/mrmatt/tests/TestNiveau.java
@@ -7,7 +7,7 @@ import jdk.jfr.StackTrace;
 public class TestNiveau {
 
     //ok
-    public static void test()throws ArrayIndexOutOfBoundsException{
+    public static void testInitNiveaux()throws ArrayIndexOutOfBoundsException{
         try {
             Niveau n = new Niveau("src/niveaux/AppleTown/1-the-market.txt");
             Niveau n1 = new Niveau("src/niveaux/AppleTown/2-apple-bins.txt");
@@ -16,28 +16,87 @@ public class TestNiveau {
 
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println(e);
+            System.out.println("Test init niveaux KO");
+        }
+        System.out.println("Test init niveaux OK");
+    }
+
+    /**
+     *
+     */
+    public static void testJouerCommandeVersMur(){
+        Niveau n = new Niveau("src/niveaux/AppleTown/3-the-attic.txt");
+
+
+        try {
+
+            n.jouer(Commande.BAS);
+
+            n.jouer(Commande.BAS);
+            n.jouer(Commande.BAS);
+            n.jouer(Commande.BAS);
+
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e + "Test Jouer commande vers mur KO");
+        }
+        System.out.println("Test Jouer commande vers mur OK");
+
+    }
+
+    public static void testPositionPostJouer(){
+        Niveau n = new Niveau("src/niveaux/AppleTown/1-the-market.txt");
+
+        int testx = n.getPositionJoueurX();
+        int testy = n.getPositionJoueurY();
+
+        n.jouer(Commande.HAUT);
+        n.jouer(Commande.DROITE);
+
+        if(testx == n.getPositionJoueurX()+1&&testy==n.getPositionJoueurY()-1){
+            System.out.println("TestPositionPostJouer OK");
+        }else {
+            System.out.println("TestPositionPostJouer KO");
         }
 
+
     }
 
-    public static void testJeu(){
-        Niveau n = new Niveau("src/niveaux/AppleTown/1-the-market.txt");
-        n.afficher();
-        System.out.println(  n.getPositionJoueurX()+" "+
-        n.getPositionJoueurY());
+    public static void deplacementPossibleEtDeplacer(){
+        Niveau n = new Niveau("src/niveaux/1rocher.txt");
+        int testPosX = n.getPositionJoueurX();
+        int testPosY= n.getPositionJoueurY();
+        n.jouer(Commande.HAUT);
+
+        n.deplacer(n.getPositionJoueurX(),n.getPositionJoueurY()+1);
+        System.out.println(testPosX +" "+n.getPositionJoueurX()+" "+n.getPositionJoueurY()+" "+testPosY);
+        if(n.getPositionJoueurX()==testPosX && testPosY+1 == n.getPositionJoueurY()){
+            System.out.println("deplacementPossible OK");
+        }else{
+            System.out.println("deplacementPossible KO");
+        }
+
+
+    }
+
+    //pas fonctionnelle car il faut tester avec la classe jeu avec encours est intermédiare etc
+    public static void testGagner(){
+        Niveau n = new Niveau("src/niveaux/1rocher.txt");
+        n.jouer(Commande.DROITE);
+        n.etatSuivant();
         n.jouer(Commande.BAS);
-        System.out.println(  n.getPositionJoueurX()+" "+
-                n.getPositionJoueurY());
-        n.afficher();
+        n.etatSuivant();
+        n.afficherEtatFinal();
+        System.out.println(n.estGagne());
     }
 
-    public static void testJeu2(){
-
-    }
 
     public static void main(String[] args) {
+        testInitNiveaux();
+        testJouerCommandeVersMur();
+        testPositionPostJouer();
+        deplacementPossibleEtDeplacer();
+        testGagner();
 
-        testJeu();
 
     }
 }
